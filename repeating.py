e2a00d935b011d80f3810f96800b2251ba2ab3ab
Ksenia Yasyukevich <ks.yasyukevich@gmail.com>
2021-06-04 12:29:40 +0300

    Update repeating.py

--- repeating.py
+++ repeating.py
@@ -8,20 +8,14 @@ def first_repeating(first_list):
     :return: first duplicate item
     """
     two_list = []
-    repeating_list = []
     if not first_list:
         return "Your list is empty."
-    else:
-        for i in first_list:
-            if i not in two_list:
-                two_list.append(i)
-            else:
-                repeating_list.append(i)
 
-    if not repeating_list:
-        return "No repeating elements."
-    else:
-        return repeating_list[0]
+    for i in first_list:
+        if i not in two_list:
+            two_list.append(i)
+        else:
+            return i
 
 
 if __name__ == '__main__':
